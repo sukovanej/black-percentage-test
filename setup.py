277bb37de98fca39c4227a58c559c2eb9b3d559e
milan.suk <milan.suk@kiwi.com>
2018-07-18 16:53:44 +0200

    setup.py description

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="black-percentage-tester",
-    description="",
+    description="Black formatter percentage checker",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version="0.0.1",
     author="Milan Suk",
     author_email="milan.suk@kiwi.com",
